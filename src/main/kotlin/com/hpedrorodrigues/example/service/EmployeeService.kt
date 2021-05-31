d5b7069403670c4374c69a55ef7951b7fb8c3831
Pedro Rodrigues <hs.pedro.rodrigues@gmail.com>
2021-05-31 16:35:50 -0300

    Adding more test cases

--- src/main/kotlin/com/hpedrorodrigues/example/service/EmployeeService.kt
+++ src/main/kotlin/com/hpedrorodrigues/example/service/EmployeeService.kt
@@ -4,6 +4,7 @@ import com.hpedrorodrigues.example.entity.Employee
 import com.hpedrorodrigues.example.repository.EmployeeRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.reactive.asFlow
 import kotlinx.coroutines.reactor.awaitSingle
 import kotlinx.coroutines.reactor.flux
 import kotlinx.coroutines.reactor.mono
@@ -20,36 +21,58 @@ class EmployeeService(private val repository: EmployeeRepository) {
   @Transactional
   fun saveMonoDSL(name: String): Mono<Employee> = mono {
     repository.save(Employee(name = name)).awaitSingle()
-    throw IllegalStateException()
+    throw RuntimeException()
   }
 
   @Transactional
   fun saveFluxDSL(name: String): Flux<Employee> = flux {
     repository.save(Employee(name = name)).awaitSingle()
-    throw IllegalStateException()
+    throw RuntimeException()
   }
 
   @Transactional
   fun saveFlowDSL(name: String): Flow<Employee> = flow {
     repository.save(Employee(name = name)).awaitSingle()
-    throw IllegalStateException()
+    throw RuntimeException()
   }
 
   @Transactional
   suspend fun saveSuspendMono(name: String): Mono<Employee> {
     repository.save(Employee(name = name)).awaitSingle()
-    throw IllegalStateException()
+    throw RuntimeException()
   }
 
   @Transactional
   suspend fun saveSuspendFlux(name: String): Flux<Employee> {
     repository.save(Employee(name = name)).awaitSingle()
-    throw IllegalStateException()
+    throw RuntimeException()
   }
 
   @Transactional
   suspend fun saveSuspendFlow(name: String): Flow<Employee> {
     repository.save(Employee(name = name)).awaitSingle()
-    throw IllegalStateException()
+    throw RuntimeException()
+  }
+
+  @Transactional
+  suspend fun saveSuspendMonoReturnAnotherMono(name: String): Mono<Employee> {
+    repository.save(Employee(name = name)).awaitSingle()
+    return Mono.error(RuntimeException())
+  }
+
+  @Transactional
+  suspend fun saveSuspendMonoReturnFlow(name: String): Flow<Employee> {
+    return saveSuspendMonoReturnAnotherMono(name).asFlow()
+  }
+
+  @Transactional
+  suspend fun saveSuspendFluxReturnAnotherFlux(name: String): Flux<Employee> {
+    repository.save(Employee(name = name)).awaitSingle()
+    return Flux.error(RuntimeException())
+  }
+
+  @Transactional
+  suspend fun saveSuspendFluxReturnFlow(name: String): Flow<Employee> {
+    return saveSuspendFluxReturnAnotherFlux(name).asFlow()
   }
 }
--- src/test/kotlin/com/hpedrorodrigues/example/ExampleApplicationTests.kt
+++ src/test/kotlin/com/hpedrorodrigues/example/ExampleApplicationTests.kt
@@ -44,7 +44,7 @@ class ExampleApplicationTests(@Autowired private val service: EmployeeService) {
   }
 
   @Test
-  fun `suspend functions mono's project reactor`() = runBlocking {
+  fun `suspend function for mono's project reactor`() = runBlocking {
     val name = "saveSuspendMono"
 
     try {
@@ -58,11 +58,67 @@ class ExampleApplicationTests(@Autowired private val service: EmployeeService) {
   }
 
   @Test
-  fun `suspend functions flux's project reactor`() = runBlocking {
+  fun `suspend function for flux's project reactor`() = runBlocking {
     val name = "saveSuspendFlux"
 
     try {
       service.saveSuspendFlux(name).collectList().awaitSingle()
+      Assertions.fail()
+    } catch (e: Exception) {
+      // Ignore error
+    }
+
+    Assertions.assertEquals(false, service.existsByName(name).awaitSingle())
+  }
+
+  @Test
+  fun `suspend function for mono's project reactor returning another mono with error`() = runBlocking {
+    val name = "saveSuspendMonoReturnAnotherMono"
+
+    try {
+      service.saveSuspendMonoReturnAnotherMono(name).awaitSingle()
+      Assertions.fail()
+    } catch (e: Exception) {
+      // Ignore error
+    }
+
+    Assertions.assertEquals(false, service.existsByName(name).awaitSingle())
+  }
+
+  @Test
+  fun `suspend function for mono's project reactor returning a flow with error`() = runBlocking {
+    val name = "saveSuspendMonoReturnFlow"
+
+    try {
+      service.saveSuspendMonoReturnFlow(name).toList()
+      Assertions.fail()
+    } catch (e: Exception) {
+      // Ignore error
+    }
+
+    Assertions.assertEquals(false, service.existsByName(name).awaitSingle())
+  }
+
+  @Test
+  fun `suspend function for flux's project reactor returning another flux with error`() = runBlocking {
+    val name = "saveSuspendFluxReturnAnotherFlux"
+
+    try {
+      service.saveSuspendFluxReturnAnotherFlux(name).collectList().awaitSingle()
+      Assertions.fail()
+    } catch (e: Exception) {
+      // Ignore error
+    }
+
+    Assertions.assertEquals(false, service.existsByName(name).awaitSingle())
+  }
+
+  @Test
+  fun `suspend function for flux's project reactor returning a flow with error`() = runBlocking {
+    val name = "saveSuspendFluxReturnFlow"
+
+    try {
+      service.saveSuspendFluxReturnFlow(name).toList()
       Assertions.fail()
     } catch (e: Exception) {
       // Ignore error
